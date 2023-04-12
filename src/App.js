9b43b6573b15953d3dfe3828346c270f894d3eeb
GLADYS <mungaiglado106@gmail.com>
2023-04-12 21:43:39 +0300

    p

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
+
+
+import Pricing from './routes/Pricing.js'
+import Contact from './routes/Contact'
+
+import Faq from './routes/Faq';
+
 function App(){
   return (
 <>
 <Routes>
 <Route path="/" element={<Home />} />
+<Route path='/pricing' element={<Pricing />} />
+      <Route path='/faq' element={<Faq />} />
+      <Route path='/contact' element={<Contact />} />
 </Routes>
 
 </>
